b288ba5687d19a27016508c1cae6d85092556ea8
tiffvan <32732343+tiffvan@users.noreply.github.com>
2019-03-28 16:26:11 +0200

    Added meal prices

--- database/seeds/MealsTableSeeder.php
+++ database/seeds/MealsTableSeeder.php
@@ -14,7 +14,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Apple pancakes',
             'ingredient_id' => ['Flour', 'Sugar', 'Cinnamon', 'Soy milk', 'Apple', 'Coconut oil'],
-            'price_id' =>  0,
+            'price_id' =>  90,
             'rating' => 4.8,
             'category' => 'Breakfasts',
             'image' => 'apple-pancakes.png'
@@ -22,7 +22,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Baked peppers',
             'ingredient_id' => ['Sweet potato', 'Flour', 'Onion', 'Pepper', 'Cherry tomato', 'Coconut milk'],
-            'price_id' =>  0,
+            'price_id' =>  115,
             'rating' => 4.5,
             'category' => 'Light meals',
             'image' => 'baked-pepper.png'
@@ -30,7 +30,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Black bean and lentil curry',
             'ingredient_id' => ['Black bean', 'Lentil', 'Sweet potato', 'Onion', 'Tomato', 'Corn'],
-            'price_id' =>  0,
+            'price_id' =>  120,
             'rating' => 4.1,
             'category' => 'Light meals',
             'image' => 'black-bean-lentil-curry.png'
@@ -38,7 +38,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Blueberry smoothie',
             'ingredient_id' => ['Blueberry', 'Coconut milk'],
-            'price_id' =>  0,
+            'price_id' =>  65,
             'rating' => 4.3,
             'category' => 'Drinks',
             'image' => 'blueberry-smoothie.png'
@@ -46,7 +46,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Braaibroodjie',
             'ingredient_id' => ['Bread', 'Onion', 'Tomato', 'Vegan mayo', 'Sweet potato'],
-            'price_id' =>  0,
+            'price_id' =>  65,
             'rating' => 4.5,
             'category' => 'Light meals',
             'image' => 'braaibroodjie.png'
@@ -54,7 +54,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Bunny chow',
             'ingredient_id' => ['Butternut', 'Onion', 'Sweet potato', 'Chickpea', 'Coconut milk', 'Bread'],
-            'price_id' =>  0,
+            'price_id' =>  105,
             'rating' => 4.7, 
             'category' => 'Light meals',
             'image' => 'bunny-chow.png'
@@ -62,7 +62,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Butternut muffin',
             'ingredient_id' => ['Flour', 'Sugar', 'Cinnamon', 'Butternut'],
-            'price_id' =>  0,
+            'price_id' =>  60,
             'rating' => 4.6,
             'category' => 'Breakfassa',
             'image' => 'butternut-muffin.png'
@@ -70,7 +70,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Cappuccino',
             'ingredient_id' => ['Coffee', 'Almond milk', 'Sugar'],
-            'price_id' =>  0,
+            'price_id' =>  45,
             'rating' => 4.7,
             'category' => 'Drinks',
             'image' => 'cappuccino.png'
@@ -78,7 +78,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Creamy potato soup',
             'ingredient_id' => ['Potato', 'Onion', 'Carrot', 'Coconut milk', 'Olive oil'],
-            'price_id' =>  0,
+            'price_id' =>  85,
             'rating' => 4.9,
             'category' => 'Light meals',
             'image' => 'potato-soup.png'
@@ -86,7 +86,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Fresh fruit juices',
             'ingredient_id' => ['Apple', 'Mango'],
-            'price_id' =>  0,
+            'price_id' =>  30,
             'rating' => 4.5,
             'category' => 'Drinks',
             'image' => 'fruit-juice.png'
@@ -94,7 +94,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Koeksisters',
             'ingredient_id' => ['Sugar', 'Cinnamon', 'Flour'],
-            'price_id' =>  0,
+            'price_id' =>  55,
             'rating' => 4.9,
             'category' => 'Desserts',
             'image' => 'koeksisters.jpg")'
@@ -102,7 +102,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Mango and berry smoothie',
             'ingredient_id' => ['Mango', 'Raspberry', 'Blueberry', 'Coconut milk'],
-            'price_id' =>  0,
+            'price_id' =>  65,
             'rating' => 4.2,
             'category' => 'Drinks',
             'image' => 'mango-berry-smoothie.png'
@@ -110,7 +110,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Melktert',
             'ingredient_id' => ['Flour', 'Coconut milk', 'Cashew', 'Cinnamon'],
-            'price_id' =>  0,
+            'price_id' =>  45,
             'rating' => 4.9,
             'category' => 'Desserts',
             'image' => 'melktert.png'
@@ -118,7 +118,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Mini meatless sausage rolls',
             'ingredient_id' => ['Flour', 'Spinach', 'Chickpea', 'Onion'],
-            'price_id' =>  0,
+            'price_id' =>  100,
             'rating' => 4.8,
             'category' => 'Light meals',
             'image' => 'mini-sausage-rolls.png'
@@ -126,7 +126,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Mushroom and spinach pizza',
             'ingredient_id' => ['Flour', 'Spinach', 'Mushroom', 'Olive oil', 'Almond'],
-            'price_id' =>  0,
+            'price_id' =>  120,
             'rating' => 4.9,
             'category' => 'Light meals',
             'image' => 'pizza.png'
@@ -134,7 +134,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Polenta fries',
             'ingredient_id' => ['Soy milk', 'Polenta', 'Olive oil', 'Tomato'],
-            'price_id' =>  0,
+            'price_id' =>  85,
             'rating' => 4.7,
             'category' => 'Light meals',
             'image' => 'polenta-fries.png'
@@ -142,7 +142,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Quinoa bowl',
             'ingredient_id' => ['Quinoa', 'Blueberry', 'Almond', 'Almond milk'],
-            'price_id' =>  0,
+            'price_id' =>  115,
             'rating' => 4.8,
             'category' => 'Breakfasts',
             'image' => 'quinoa-bowl.png'
@@ -150,7 +150,7 @@ class MealsTableSeeder extends Seeder
         DB::table('meals')->insert([
             'name' => 'Scones',
             'ingredient_id' => ['Flour', 'Vegan mayo', 'Soy milk', 'Raspberry'],
-            'price_id' =>  0,
+            'price_id' =>  75,
             'rating' => 4.5,
             'category' => 'Breakfasts',
             'image' => 'scones.png'
