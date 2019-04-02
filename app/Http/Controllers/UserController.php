af0c81b686fa79d3827098cc10d91d405ff4904f
tiffvan <32732343+tiffvan@users.noreply.github.com>
2019-04-02 10:11:19 +0200

    Admin user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\User;
+
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(InventoryTableSeeder::class);
         $this->call(MealsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
