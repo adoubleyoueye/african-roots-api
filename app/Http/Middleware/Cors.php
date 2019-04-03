30a38e0a3923d2121d843c379c4baa1f53ba55d0
Awi <160194@virtualwindow.co.za>
2019-04-03 11:17:42 +0200

    Test and Implement CORS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,35 +1,31 @@
-<?php namespace App\Http\Middleware;
-
+<?php
+namespace App\Http\Middleware;
 use Closure;
-
-class CORS {
-
-   /**
-    * Handle an incoming request.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \Closure  $next
-    * @return mixed
-    */
-   public function handle($request, Closure $next)
-   {
-
-       header("Access-Control-Allow-Origin: *");
-
-       // ALLOW OPTIONS METHOD
-       $headers = [
-           'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-           'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-       ];
-       if($request->getMethod() == "OPTIONS") {
-           // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-           return Response::make('OK', 200, $headers);
-       }
-
-       $response = $next($request);
-       foreach($headers as $key => $value)
-           $response->header($key, $value);
-       return $response;
-   }
-
+/**
+ * Class Cors
+ * @package App\Http\Middleware
+ */
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * Please add header('Access-Control-Allow-Origin: http://example.com');
+     * & header('Access-Control-Allow-Credentials: true');
+     * at the top of your route file.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->isMethod('options')) {
+            return response('', 200)
+              ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+              ->header('Access-Control-Allow-Headers', 'accept, content-type,
+                x-xsrf-token, x-csrf-token'); // Add any required headers here
+        }
+        return $next($request);
+    }
 }
